a3acbbe1201fc124b5b7a7bfa5a03cba276616b7
Erkebulan Safiolla <erkebulan91@gmail.com>
2023-01-15 00:30:15 +0600

    Signed-off-by: Erkebulan Safiolla <erkebulan91@gmail.com>

---
+++ app/js/main.js
@@ -0,0 +1,44 @@
+//dropdown menu in main-navigation
+$('.main-nav-item').mouseenter(function () {
+  if($(window).width() > 991) {
+    $(this).addClass('active')
+  }
+});
+$('.main-nav-item').mouseleave(function () {
+  if($(window).width() > 991) {
+    $(this).removeClass('active')
+  }
+});
+$('.main-nav-item').click(function (){
+  if ($(window).width() < 992) {
+    $(this).toggleClass('active')
+  }
+})
+
+
+//show mobile menu
+$('.toggle-mobile-menu').click(function (){
+  $(this).toggleClass('active');
+  $('.header-main').toggleClass('show');
+  $('body').toggleClass('scroll-locked');
+  let height = $('.header').innerHeight();
+  let menuHeight = $(window).height() - height;
+  $('.header-main').css({
+    'top': height,
+    'height': menuHeight
+  })
+});
+
+
+//switch lang
+$('.switch-lang a').click(function () {
+  $('.switch-lang a').removeClass('active');
+  $(this).addClass('active');
+})
+
+
+//footer menu show on mobile
+$('.footer-col-title p').click(function (){
+  $(this).toggleClass('active')
+  $(this).closest('.footer-col-title').next('.footer-col-list').slideToggle()
+})
